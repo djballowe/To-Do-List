3a6af85b96b7de90fa0b4eceaab211acbb95dc24
djballowe <djballowe@gmail.com>
2022-06-09 12:21:50 -0700

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   mode: 'development',
+   entry: {
+
+    index: './src/index.js',
+
+    print: './src/print.js',
+
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+
+    static: './dist',
+
+  },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      title: 'Output Management',
+
+    }),
+
+  ],
+    output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+
+    runtimeChunk: 'single',
+
+  },
+};
