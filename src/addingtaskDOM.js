dca51eea24a052ce402f00ead664eeaa27b30209
djballowe <djballowe@gmail.com>
2022-06-13 16:51:15 -0700

    object details adding to the dom

--- src/addingtaskDOM.js
+++ src/addingtaskDOM.js
@@ -19,18 +19,22 @@ function taskDOM() {
     for (let i = 0; i < arr.length; i++) {
         let index = arr.length - 1;
 
-        document.querySelector('.tasks').appendChild(elementFactory('div', {class: 'new-task'}));
+        document.querySelector('.tasks').appendChild(elementFactory('div', {class: `new-task`, id: `task-${index}`}));
 
         document.querySelector('.new-task').appendChild(elementFactory('div', {class: 'left-side'}));
 
-        document.querySelector('.left-side').appendChild(elementFactory('button', {class: 'check', type: 'button'}));
-        document.querySelector('.left-side').appendChild(elementFactory('div', {class: 'project-text'}))
+        const left = document.querySelector('.left-side');
+        left.appendChild(elementFactory('button', {class: 'check', type: 'button'}));
+        left.appendChild(elementFactory('div', {class: 'project-text'}))
 
-        document.querySelector('.project-text').appendChild(elementFactory('p', 'none', ));
-        document.querySelector('.project-text').appendChild(elementFactory('p', 'none', ));
-        console.log(arr);
+        document.querySelector('.project-text').appendChild(elementFactory('p', 'none', `${arr[index].title}`));
+        document.querySelector('.project-text').appendChild(elementFactory('p', 'none', `${arr[index].description}`));
+        
+        document.querySelector('.new-task').appendChild(elementFactory('div', {class: 'info'}))
+        const info = document.querySelector('.info');
+        info.appendChild(elementFactory('p', 'none', `${arr[index].date}`));
+
         break;
-
     }
 }
 
