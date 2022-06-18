019b9b5de1d0ed11129879ca6f18be1165876b35
djballowe <djballowe@gmail.com>
2022-06-18 09:15:54 -0700

    fixed bug where adding a space in a project field would result in an error

--- src/addingProjectDOM.js
+++ src/addingProjectDOM.js
@@ -13,10 +13,16 @@ function getProjects() {
             class: 'icons'
         }
     }
+
+    const newProject = document.getElementById('project-input').value;
+    const newProjectNoSpaces = newProject.replaceAll(' ', '-');
     
-    projects.push(`${document.getElementById('project-input').value}`);
+    
+    projects.push(newProjectNoSpaces);
+    
+    
     iconClass.push(
-        catCreator(`project-${document.getElementById('project-input').value}`)
+        catCreator(`project-${newProjectNoSpaces}`)
     )
     return { catCreator }
 }
@@ -29,19 +35,21 @@ function addProjectToStorage() {
 function projectDom() {
     const container = document.querySelector('.project-select');
     const newCatContainer = document.querySelector('.all');
+    
 
     projects.forEach((item, index) => {
+        const text = projects[index].replaceAll('-', ' ');
         container.appendChild(elementFactory('div', {class: 'new-project', id: `project-${projects[index]}`, data: 'organize'}));
         document.querySelector(`#project-${projects[index]}`).appendChild(elementFactory('div', {id: `name-${index}`, class: 'name'}));
         document.querySelector(`#name-${index}`).appendChild(elementFactory('div', {id: `grid-${index}`, index: `${index}`, class: 'grid'}));
         document.querySelector(`#grid-${index}`).appendChild(elementFactory('img', {src: '../src/grid.svg', index: `${index}`}));
-        document.querySelector(`#name-${index}`).appendChild(elementFactory('p', 'none', `${projects[index]}`));
+        document.querySelector(`#name-${index}`).appendChild(elementFactory('p', 'none', `${text}`));
         document.querySelector(`#project-${projects[index]}`).appendChild(elementFactory('div', {id: `trash-${index}`, class: 'trash'}));
         document.querySelector(`#trash-${index}`).appendChild(elementFactory('img', {src: '../src/trash.svg', index: `${index}`}));
 
         // make a new category title
 
-        newCatContainer.appendChild(elementFactory('h2', {id: `cat-project-${projects[index]}`}, `${projects[index]}`));
+        newCatContainer.appendChild(elementFactory('h2', {id: `cat-project-${projects[index]}`}, `${text}`));
         document.getElementById(`cat-project-${projects[index]}`).style.display = 'none';
     })
 }
