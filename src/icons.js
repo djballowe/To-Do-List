5521004b5a515a446560656627a3ec9e742d92d7
djballowe <djballowe@gmail.com>
2022-06-19 08:05:56 -0700

    fixed double click highlight bug

--- src/icons.js
+++ src/icons.js
@@ -96,38 +96,40 @@ function catagories() {
 const cat = catagories();
 
 function focusEffects(click, index) {
-    console.log(iconClass);
+    
     const icon = document.getElementById(click);
     const active = iconClass.find(e => e.class === 'icons-active');
     const nowActive = iconClass.find(e => e.id === `${click}`);
     const highlight = document.getElementById(`${active.id}`);
     
-    icon.classList.add('icons-active');
-    highlight.classList.toggle('icons-active');
-    active.class = 'icons';
-    nowActive.class = 'icons-active';
-
-    // switch the category visually along with the focus effect
     
-    const currentCat = document.getElementById(`cat-${nowActive.id}`);
-    const pCat = document.getElementById(`cat-${active.id}`);
+        
+        active.class = 'icons';
+        nowActive.class = 'icons-active';
+        icon.removeAttribute('class');
+        icon.classList.add(nowActive.class);
+        highlight.removeAttribute('class');
+        highlight.classList.add(active.class);
 
-    console.log(currentCat);
-    
-    currentCat.style.display = 'block';
-    pCat.style.display = 'none';
+        // switch the category visually along with the focus effect
+        
+        const currentCat = document.getElementById(`cat-${nowActive.id}`);
+        const pCat = document.getElementById(`cat-${active.id}`);
+        
+        currentCat.style.display = 'block';
+        pCat.style.display = 'none';
 
-    if (nowActive.id === 'project-important') {
-        cat.stared();
-    } else if (nowActive.id === 'project-all') {
-        cat.all();
-    } else if (nowActive.id === 'project-today') {
-        cat.today();
-    } else if (nowActive.id === 'project-upcoming') {
-        cat.upcoming();
-    } else if (nowActive.id === `project-${projects[index]}`) {
-        cat.dynamicProjects(projects[index]);
-    } 
+        if (nowActive.id === 'project-important') {
+            cat.stared();
+        } else if (nowActive.id === 'project-all') {
+            cat.all();
+        } else if (nowActive.id === 'project-today') {
+            cat.today();
+        } else if (nowActive.id === 'project-upcoming') {
+            cat.upcoming();
+        } else if (nowActive.id === `project-${projects[index]}`) {
+            cat.dynamicProjects(projects[index]);
+        } 
 }
 
 export { focusEffects, catagories };
